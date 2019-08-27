a6c756c165c8f0606b33d5848a3b7eb187b1a58e
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-08-27 12:17:22 +0200

    feat: 7. Exercise: Creating and Adding A Fragment
    Other changes: added an UI test for the MainActivity using Espresso.

---
+++ app/src/androidTest/java/com/example/android/navigation/MainActivityTest.kt
@@ -0,0 +1,27 @@
+package com.example.android.navigation;
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun onStartUp_showCorrectFragment() {
+        onView(withId(R.id.fragment_title)).check(matches(isDisplayed()))
+    }
+}
+
+
