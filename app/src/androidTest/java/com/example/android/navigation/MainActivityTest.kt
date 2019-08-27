c578cc7835f7524227a92ebbc7ac9b2835fa9b2d
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-08-27 15:20:28 +0200

    feat: 17. Exercise: Adding a Menu Changes from original solution ------------------------------ * Used more appropriate id for the About Fragment in the navigation.xml * Added Espresso test for navigating to AboutFragment

--- app/src/androidTest/java/com/example/android/navigation/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/android/navigation/MainActivityTest.kt
@@ -1,12 +1,13 @@
-package com.example.android.navigation;
+package com.example.android.navigation
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
@@ -28,6 +29,13 @@ class MainActivityTest {
     fun onClickPlay_goToGameFragment() {
         onView(withId(R.id.playButton)).perform(click())
         onView(withId(R.id.scrollview_game)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun onAboutMenuClicked_goToAboutFragment() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().targetContext)
+        onView(withText("About")).perform(click())
+        onView(withId(R.id.constraint_about)).check(matches(isDisplayed()))
     }
 }
 
