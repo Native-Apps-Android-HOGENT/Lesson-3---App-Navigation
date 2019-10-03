317cab328615bf3f28ac6aba6ba701dbec075467
eothein <jens.buysse@gmail.com>
2019-10-03 10:45:47 +0200

    [refactor] Create GameViewModel

--- app/src/main/java/com/example/android/navigation/game/Question.kt
+++ app/src/main/java/com/example/android/navigation/game/Question.kt
@@ -2,7 +2,7 @@ package com.example.android.navigation.game
 
 /**
  * A data class representing a question, with its corresponding possible answers.
- * The first answers in the list is the correct answer for the question. 
+ * The first answers in the list is the correct answer for the question.
  */
 data class Question(val text: String,
                     val answers: List<String>);
